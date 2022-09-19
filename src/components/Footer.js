a0c902c085350483419c24a96157974aa349de42
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2022-09-19 14:54:57 -0400

    created components and added an image

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Footer = () => {
+    return(
+        <div className="Footer">
+          <h3> Contact Me </h3>
+          <h3>Email : yannickwessidjam@gmail.com</h3>
+          <h3>Linkedin : https://www.Linkedin.com/in/yannickw</h3>
+          <h3>Github : https://github.com/YanniWessi</h3>
+
+
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Projects.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Projects = () => {
+    return (
+      <div className="Projects">
+
+        <p>link</p>
+
+        <p>link</p>
+
+        <p>link</p>
+
+        <p>link</p>
+
+        <p>link</p>
+
+      </div>
+    )
+}
+
+export default Projects
